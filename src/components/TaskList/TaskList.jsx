a7f1088ef26e9c901fd5619efec42ef0e8b1ecea
Arkapravo01 <arkapravochakrabarti188@gmail.com>
2025-04-02 16:54:38 +0530

    Init

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,25 +4,26 @@ import NewTask from "./NewTask";
 import CompleteTask from "./CompleteTask";
 import FailedTask from "./FailedTask";
 
-const TaskList = ({data}) => {
+const TaskList = ({ data, setUserData }) => {
   return (
     <div
       id="tasklist"
-      className=" overflow-x-auto h-[55%]  flex items-center justify-start gap-5 flex-nowrap w-full py-5  mt-10"
+      className="overflow-x-auto h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10"
     >
-      {data.tasks.map((elem,idx)=>{
-        if(elem.active){
-            return <AcceptTask key={idx} data={elem}/>
+      {data.tasks.map((elem, idx) => {
+        if (elem.active) {
+          return <AcceptTask key={idx} data={elem} setUserData={setUserData} />;
         }
-        if(elem.newTask){
-            return <NewTask key={idx} data={elem}/>
+        if (elem.newTask) {
+          return <NewTask key={idx} data={elem} setUserData={setUserData} />;
         }
-        if(elem.completed){
-            return <CompleteTask key={idx} data={elem}/>
+        if (elem.completed) {
+          return <CompleteTask key={idx} data={elem} />;
         }
-        if(elem.failed){
-            return <FailedTask key={idx} data={elem}/>
+        if (elem.failed) {
+          return <FailedTask key={idx} data={elem} />;
         }
+        return null;
       })}
     </div>
   );
